49d9e6abee64c07f99441b24dd23137b9361a5cf
Ashith <ashith.madan@experionglobal.com>
2016-12-30 18:03:02 +0530

    not final

---
+++ miijs.js
@@ -0,0 +1,21 @@
+  else if ($('#time').val() != "")
+         {
+          if( ($('#time').val().length === 4){
+            time=$('#time').val();
+            var time1;
+            time1 =time.charAt(0)+time.charAt(1)+':'+time.charAt(2)+time.charAt(3)
+          time=
+        }
+       }
+
+       
+
+       for(i=0;i<a.length;i++)
+    {
+        if('A' <= a[i] && a[i] <= 'Z') // check if you have an uppercase
+            b++;
+        if('a' <= a[i] && a[i] <= 'z') // check if you have a lowercase
+            c++;
+        if('0' <= a[i] && a[i] <= '9') // check if you have a numeric
+            d++;
+    }
--- my.js
+++ my.js
@@ -1,30 +1,79 @@
 $(function() {
 
-    var ids = new Array();
-
+var userdata=new Array();
+var user = new Array();
+var myObj={};
+var myJSON={};
+var refnum;
+var date;
+var time;
+var acctype;
+var city;
+var district;
+var noofveh;
+var noofcas;
+var repofname;
     $("#datepicker-13").datepicker();
     $("#datepicker-13").datepicker("show");
+
     $("#add").click(function(e) {
         e.preventDefault();
-        if ($('#id').val() == "") {
-            alert('Please fill the id field');
-        } else if ($('#name').val() == "") {
-            alert('Please fill the name field');
-        } else if ($('#datepicker-13').val() == "") {
-            alert('Please select Date of Birth');
-        } else if ($('#empDept').val() == "select") {
-            alert('Please select the Department');
-        } else {
-            var eid = $("#id").val();
-            var name = $("#name").val();
-            var dob = $("#datepicker-13").val();
-            var dept = $("#empDept").val();
+         var ids = new Array();
+        if ($('#refnum').val() == "") {
+            alert('Please fill the Reference Number');
+        }
+        else if ($('#datepicker-13').val() == "")
+       {
+           alert('Please select Date of Accident');
+       }
+         else if ($('#time').val() == "")
+          {
+            alert('Please fill the time field');
+        }
+        else if ($('#acctype').val() == "select")
+        {
+            alert('Please select the type of Accident');
+        }
+        else if ($('#city').val() == "")
+         {
+           alert('Please fill the city field');
+       }
+       else if ($('#district').val() == "select")
+       {
+           alert('Please select the District');
+       }
+       else if ($('#noofveh').val() == "")
+        {
+          alert('Please fill the Number of Vehicles');
+      }
+      else if ($('#noofcas').val() == "")
+       {
+         alert('Please fill the Number of Casuality');
+     }
 
-            console.log(eid);
-            console.log(name);
-            console.log(dob);
-            console.log(dept);
+     else {
+             refnum = $("#refnum").val();
+             date = $("#datepicker-13").val();
+             time = $("#time").val();
+             acctype = $("#acctype").val();
+             city = $("#city").val();
+             district = $("#district").val();
+             noofveh = $("#noofveh").val();
+             noofcas = $("#noofcas").val();
+             repofname = $("#repofname").val();
 
+
+
+            console.log(refnum);
+            console.log(date);
+            console.log(time);
+            console.log(acctype);
+
+            console.log(city);
+            console.log(district);
+            console.log(noofveh);
+            console.log(repofname);
+//<div *ngIf="f.submitted && !password.valid" class="help-block">Password is required</div>
             var i;
             var flag = 0;
             console.log(ids.length);
@@ -40,10 +89,10 @@ $(function() {
             }
 
             if (flag === 0) {
-                $("#table").append('<tr><td>' + eid + '</td><td>' + name + '</td><td>' + dob + '</td><td>' + dept + '</td></tr>');
+                $("#table").append('<tr><td>' + refnum + '</td><td>' + date + '</td><td>' + time + '</td><td>' + acctype +  '</td></td>' + city + '</td><td>' + district + '</td><td>' + noofveh + '</td><td>' + noofcas +  '</td><td>' + repofname + '<td><tr>');
                 ids.push(eid);
 
-                
+
                 $( "#dialog-message" ).dialog(
             {
               modal: true,
@@ -59,6 +108,13 @@ $(function() {
             }
         }
     });
-
-
+    $("#add1").click(function(e) {
+        e.preventDefault();
+    myObj = { "id":refnum, "date":date,"time":time, "acctype":acctype,"city":city,
+     "district":district, "noofveh":noofveh,"noofcas":noofcas};
+    userdata.push(myObj);
+    myJSON = JSON.stringify(userdata);
+    localStorage.setItem("user", myJSON);
+    alert('Data successfully saved in localStorage');
+});
 });
